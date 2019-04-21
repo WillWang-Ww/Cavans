3ff862560f5ba99696c04f13fa44e51bb8d1890b
WILLWANG <wangziweichn@outlook.com>
2019-04-21 20:58:01 +0800

    增加移动设备的监听

--- main.js
+++ main.js
@@ -1,66 +1,102 @@
 var yyy = document.getElementById('xxx');
-    var context = yyy.getContext('2d');
+var context = yyy.getContext('2d');
 
-    autoSetCanvasSize(yyy)
+autoSetCanvasSize(yyy)
 
-    listenToMouse(yyy)
-
-
-    var eraserEnabled = false
-    eraser.onclick = function () {
-        eraserEnabled = true
-        actions.className = 'actions x'
-
-    }
-    brush.onclick = function () {
-        eraserEnabled = false
-        actions.className = 'actions'
-    }
+listenToUser(yyy)
 
 
-    /******/
+var eraserEnabled = false
+eraser.onclick = function () {
+    eraserEnabled = true
+    actions.className = 'actions x'
 
-    function autoSetCanvasSize(canvas) {
+}
+brush.onclick = function () {
+    eraserEnabled = false
+    actions.className = 'actions'
+}
+
+
+/******/
+
+function autoSetCanvasSize(canvas) {
+    setCanvasSize()
+
+    window.onresize = function () {
         setCanvasSize()
-
-        window.onresize = function () {
-            setCanvasSize()
-        }
-
-        function setCanvasSize() {
-            var pageWidth = document.documentElement.clientWidth
-            var pageHeight = document.documentElement.clientHeight
-
-            canvas.width = pageWidth
-            canvas.height = pageHeight
-        }
     }
 
-    function drawCircle(x, y, radius) {
-        context.beginPath()
-        context.fillStyle = 'black'
-        context.arc(x, y, radius, 0, Math.PI * 2);
-        context.fill()
+    function setCanvasSize() {
+        var pageWidth = document.documentElement.clientWidth
+        var pageHeight = document.documentElement.clientHeight
+
+        canvas.width = pageWidth
+        canvas.height = pageHeight
+    }
+}
+
+function drawCircle(x, y, radius) {
+    context.beginPath()
+    context.fillStyle = 'black'
+    context.arc(x, y, radius, 0, Math.PI * 2);
+    context.fill()
+}
+
+function drawLine(x1, y1, x2, y2) {
+    context.beginPath();
+    context.strokeStyle = 'black'
+    context.moveTo(x1, y1) // 起点
+    context.lineWidth = 5
+    context.lineTo(x2, y2) // 终点
+    context.stroke()
+    context.closePath()
+}
+
+function listenToUser(canvas) {
+
+
+    var using = false
+    var lastPoint = {
+        x: undefined,
+        y: undefined
     }
 
-    function drawLine(x1, y1, x2, y2) {
-        context.beginPath();
-        context.strokeStyle = 'black'
-        context.moveTo(x1, y1) // 起点
-        context.lineWidth = 5
-        context.lineTo(x2, y2) // 终点
-        context.stroke()
-        context.closePath()
-    }
-
-    function listenToMouse(canvas) {
-
-
-        var using = false
-        var lastPoint = {
-            x: undefined,
-            y: undefined
+    if (document.body.ontouchstart !== undefined) {  //特性检测
+        canvas.ontouchstart = function () {
+            var x = aaa.touches[0].clientX
+            var y = aaa.touches[0].clientY
+            using = true
+            if (eraserEnabled) {
+                context.clearRect(x - 5, y - 5, 10, 10)
+            } else {
+                lastPoint = {
+                    "x": x,
+                    "y": y
+                }
+            }
         }
+        canvas.ontouchmove = function () {
+            var x = aaa.touches[0].clientX
+            var y = aaa.touches[0].clientY
+
+            if (!using) { return }
+
+            if (eraserEnabled) {
+                context.clearRect(x - 5, y - 5, 10, 10)
+            } else {
+                var newPoint = {
+                    "x": x,
+                    "y": y
+                }
+                drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+                lastPoint = newPoint
+            }
+        }
+        canvas.ontouchend = function () {
+            using = false
+        }
+    } else {
         canvas.onmousedown = function (aaa) {
             var x = aaa.clientX
             var y = aaa.clientY
@@ -96,3 +132,12 @@ var yyy = document.getElementById('xxx');
             using = false
         }
     }
+
+}
+
+canvas.ontouchstart = function () {
+}
+canvas.ontouchmove = function () {
+}
+canvas.ontouchend = function () {
+}
